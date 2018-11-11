1bc73e42e1d63554680db66fcc68fa0c3c4de215
gohuygo <huy@gohuygo.com>
2018-11-10 16:57:36 -0800

    Revert my changes

--- contracts/HealthcareLogic.sol
+++ contracts/HealthcareLogic.sol
@@ -1,33 +1,24 @@
 pragma solidity ^0.4.23;
 
-import { HealthcareStorage } from "./HealthcareStorage.sol";
+// import { HealthcareStorage } from "./HealthcareStorage.sol";
 
 // Logic contract - Upgradeable
 contract HealthcareLogic {
-  address public storageAddress;
-  address public owner;
-
-  constructor(address _storageAddress) public {
-    storageAddress = _storageAddress;
-    owner = msg.sender;
+  struct User{
+    uint8  age;
+    string name;
+    string history;
   }
 
-  function updateContract(address _storageAddress) public returns (bool) {
-    require(msg.sender == owner);
-    storageAddress = _storageAddress;
+  function writeData(uint8 _age, string _name, string _history, address _sender) public returns (bool) {
+    User memory newData = User(_age, _name, _history);
+    userData[_sender] = newData;
 
     return true;
   }
 
-
-  function writeUserData(uint8 _age, string _name, string _history) public returns (bool){
-    address(storageAddress).call(bytes4(keccak256("writeUserData(uint8,string,string)",_age,_name,_history)));
-    return true;
+  function readData(address _patient) public returns (string) {
+    User memory data =  userData[_patient];
+    return data.history;
   }
-
-  function readUserData(address _patient) public returns (string) {
-    return address(storageAddress).call(bytes4(keccak256("readUserData(address)",_patient)));
-  }
-
-
 }
